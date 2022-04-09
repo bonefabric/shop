0764b889574c682a6c00be581ba38814da56ce3c
BoneFabric <iburanguloff@ya.ru>
2022-04-10 01:12:00 +0500

    routes protection

--- resources/js/components/Root.vue
+++ resources/js/components/Root.vue
@@ -7,6 +7,7 @@
 import {useStore} from "vuex";
 import {computed, onBeforeMount, ref} from "vue";
 import {useRouter} from "vue-router";
+import {addFirewall} from "../router";
 
 const store = useStore();
 const router = useRouter();
@@ -17,6 +18,18 @@ const loading = ref(true)
 onBeforeMount(async () => {
     await store.dispatch('system/getCsrfToken');
     await store.dispatch('auth/loadUser');
+    addFirewall(store);
     loading.value = false;
+
+    const currentRoute = router.currentRoute.value;
+
+    if (currentRoute.meta.access) {
+        if (currentRoute.meta.access.authorized && !store.state.auth.authorized) {
+            await router.push({name: 'login'});
+        } else if (currentRoute.meta.access.guest && store.state.auth.authorized) {
+            await router.push({name: 'index'});
+        }
+    }
+
 });
 </script>
--- resources/js/router/index.js
+++ resources/js/router/index.js
@@ -1,6 +1,13 @@
 import {createRouter, createWebHistory} from "vue-router";
 import Index from "../components/views/Index";
 
+const access = {
+    guestOnly: {
+        authorized: false,
+        guest: true,
+    },
+}
+
 const routes = [
     {
         name: 'index',
@@ -13,6 +20,7 @@ const routes = [
         component: () => import("../components/views/Register"),
         meta: {
             layout: 'dark-layout',
+            access: access.guestOnly,
         },
     },
     {
@@ -21,6 +29,7 @@ const routes = [
         component: () => import("../components/views/Login"),
         meta: {
             layout: 'dark-layout',
+            access: access.guestOnly,
         },
     },
 ];
@@ -29,5 +38,22 @@ const router = createRouter({
     history: createWebHistory(),
     routes: routes,
 });
+
+export const addFirewall = function (store) {
+    router.beforeEach((to, from, next) => {
+        to.matched.forEach(route => {
+            if (route.meta.access) {
+                if (route.meta.access.authorized && !store.state.auth.authorized) {
+                    next({name: 'login'});
+                    return;
+                } else if (route.meta.access.guest && store.state.auth.authorized) {
+                    next(false);
+                    return;
+                }
+            }
+            next();
+        });
+    });
+}
 
 export default router;
