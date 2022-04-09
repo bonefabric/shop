b24241bed82aeba8a545edeeb9410fd6a7ee9077
BoneFabric <iburanguloff@ya.ru>
2022-04-09 18:42:06 +0500

    api & controllers

---
+++ app/Http/Controllers/Api/V1/AuthController.php
@@ -0,0 +1,9 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Http\Controllers\Api\V1;
+
+class AuthController extends ApiV1Controller
+{
+
+}
--- routes/api.php
+++ routes/api.php
@@ -1,19 +1,15 @@
 <?php
+declare(strict_types=1);
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+Route::group(['prefix' => 'v1'], function () {
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+    Route::group(['middleware' => 'auth:sanctum'], function () {
+
+        Route::get('/user', function (Request $request) {
+            return $request->user();
+        });
+    });
 });
